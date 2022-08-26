36b00aa18e18d11bbe9ee04d59d538db58637824
jacobchestnut <e16.jmc@gmail.com>
2022-08-26 17:14:00 -0400

    version 1.0

--- src/PanelHandler.java
+++ src/PanelHandler.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.lang.reflect.Field;
 
 public class PanelHandler extends JPanel {
-    JButton[] buttons;
-    Icon icon = new ImageIcon("dir.png");
-    Icon emptyIcon = new ImageIcon("emptyDir.png");
+    private JButton[] buttons;
+    private Icon icon = new ImageIcon("dir.png");
+    private Icon emptyIcon = new ImageIcon("emptyDir.png");
+    private Icon fileIcon = new ImageIcon("file.png");
+    private Icon txtIcon = new ImageIcon("txtFile.png");
     public PanelHandler(File[] dir){
+
         buttons = new JButton[dir.length];
         for (int i = 0; i < dir.length; i++) {
             if (dir[i].isDirectory()) {
@@ -18,10 +21,18 @@ public class PanelHandler extends JPanel {
                     buttons[i] = new JButton(dir[i].getName(), icon);
                 }
             }else{
-                buttons[i] = new JButton(dir[i].getName());
+                try {
+                    if (dir[i].getName().substring(dir[i].getName().length() - 6, dir[i].getName().length() - 1).equalsIgnoreCase(".txt")) {
+                        buttons[i] = new JButton(dir[i].getName(), txtIcon);
+                    } else
+                        buttons[i] = new JButton(dir[i].getName(), fileIcon);
+                } catch (StringIndexOutOfBoundsException e){
+                    buttons[i] = new JButton(dir[i].getName(), fileIcon);
+                }
             }
             buttons[i].setMinimumSize(new Dimension(16,16));
-            buttons[i].setMaximumSize(new Dimension(200,250));
+            buttons[i].setMaximumSize(new Dimension(200,200));
+            buttons[i].setPreferredSize(new Dimension(200,200));
             super.add(buttons[i]);
         }
     }
@@ -29,5 +40,6 @@ public class PanelHandler extends JPanel {
     public JButton[] getButtons(){
         return buttons;
     }
+
 
 }
